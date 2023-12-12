1d9c4303fa250ac4b6c8ad5cb4e182d699af4e84
Diffelbra <95624104+Diffelbra@users.noreply.github.com>
2023-12-12 14:50:19 -0500

    Completes Lab 5

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-let firstName = "Michael";
-console.log("My name is " + firstName);
 
-let favNumber = 6;
-let squared = favNumber * favNumber;
-console.log(`The square of ${favNumber} is ${squared}.`);
+class Book {
+    constructor(title, author, genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+}
 
-let x = 10;
-let y = 20;
 
-let sum = x + y;
-console.log('The sum of x and y is: ', sum);
+let books = [];
 
-let product = x * y;
-console.log('The product of x and y is: ', product);
+function addBook(title, author, genre) {
+    let book = new Book(title, author, genre);
+    books.push(book);
+    console.log(`Book titled '${title}' by ${author} has been added to the catalogue.`);
+}
+
+function showBooks() {
+    books.forEach((book, index) => {
+        console.log(`Index: ${index}, Title: ${book.title}`);
+    });
+}
+
+function showBook(index) {
+    if (index >= 0 && index < books.length) {
+        let book = books[index];
+        console.log(`Title: ${book.title}, Author: ${book.author}, Genre: ${book.genre}`);
+    } else {
+        console.log('Invalid index. No such book in the catalogue.');
+    }
+}
