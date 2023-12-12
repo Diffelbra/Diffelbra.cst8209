122a905ebfd5c068d432f1d9d6eba1bae8cadf34
Diffelbra <95624104+Diffelbra@users.noreply.github.com>
2023-12-12 13:50:56 -0500

    lab-2

---
+++ javascipt.js
@@ -0,0 +1,29 @@
+let firstName = "YourFirstName"; // replace with your first name
+console.log("My name is " + firstName);
+
+let firstName = "Michael";
+console.log("My name is " + firstName);
+
+let favNumber = 6;
+let squared = favNumber * favNumber;
+console.log(`The square of ${favNumber} is ${squared}.`);
+
+// Declare variables
+let x = 10;
+let y = 20;
+
+// Add x and y
+let sum = x + y;
+
+// Log sum to the console
+console.log('The sum of x and y is: ', sum);
+
+// Multiply x and y
+let product = x * y;
+
+// Log product to the console
+console.log('The product of x and y is: ', product);
+
+let favNumber = 7; // replace with your favorite number
+let squared = favNumber * favNumber;
+console.log(`The square of ${favNumber} is ${squared}.`);
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function setTitle(newTitle) {
-    const pageTitle = document.getElementById('pageTitle');
-    pageTitle.innerText = newTitle;
-    return 'Title has been updated to: $(newTitle)';
-}
+let firstName = "Michael";
+console.log("My name is " + firstName);
 
-setTitle("<h1>");
-increaseFontSize("<h1>");
-decreaseFontSize("<h1>");
-setBackgroundColor("lightyellow");
-setFontColor("darkblue");
-setTheme("theme2");
+let favNumber = 6;
+let squared = favNumber * favNumber;
+console.log(`The square of ${favNumber} is ${squared}.`);
+
+let x = 10;
+let y = 20;
+
+let sum = x + y;
+console.log('The sum of x and y is: ', sum);
+
+let product = x * y;
+console.log('The product of x and y is: ', product);
